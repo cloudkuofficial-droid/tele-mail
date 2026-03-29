57fe9cee4d291fa4b58ff71e45189944d3b09eb1
cloudkuofficial-droid <cloudku.official@gmail.com>
2026-03-29 18:41:05 +0700

    Add files via upload

---
+++ api/webhook.js
@@ -0,0 +1,29 @@
+// file: api/webhook.js
+const TelegramBot = require('node-telegram-bot-api');
+const mongoose = require('mongoose');
+const { token, mongoURI } = require('../setting');
+const { setBotInstance } = require('../index');
+
+let isDbConnected = false;
+const connectDb = async () => {
+    if (isDbConnected) return;
+    await mongoose.connect(mongoURI);
+    isDbConnected = true;
+};
+
+// BOT TANPA POLLING (Mode Webhook)
+const bot = new TelegramBot(token); 
+setBotInstance(bot);
+
+module.exports = async (req, res) => {
+    try {
+        await connectDb(); // Sambungkan ke MongoDB
+        if (req.body) {
+            await bot.processUpdate(req.body); // Serahkan pesan ke index.js
+        }
+        res.status(200).send('OK');
+    } catch (error) {
+        console.error("Webhook Error:", error);
+        res.status(500).send('Error');
+    }
+};
